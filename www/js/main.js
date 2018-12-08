b6b2269f42c020911b779961c86b73a17f81161b
sheeptipper <mzhang527@gmail.com>
2018-12-08 14:59:52 -0500

    testing code

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,16 @@
 let a
+var have_reference_location;
+var reference_coords;
+var threshold;
+var textCounter;
+var textCounter2;
+var distance;
+var textToPrint;
+var textToPrint2;
+var accel;
+var markov;
+var accuracy;
+var r, g, b;
 
 fetch('Nietzsche.txt')
   .then(function(response) {
@@ -6,14 +18,27 @@ fetch('Nietzsche.txt')
   })
   .then(function(text) {
     a = text
+    //code to set up rita markov chain model
+    markov = new RiMarkov(4);
+    markov.loadText(a);
+
   });
 
-
-function setup() {
-
+ function setup() {
+  createCanvas(window.innerWidth, window.innerHeight, P2D);
+  r = random(50, 255);
+  g = random(0, 200);
+  b = random(50, 255);
 }
 
 function draw() {
-  text('I am', 50, 80);
-  textSize(60);
+  background(r, g, b);
+  console.log('draw');
+}
+
+function deviceMoved() {
+    r = random(50, 255);
+    g = random(0, 200);
+    b = random(50, 255);
+
 }
