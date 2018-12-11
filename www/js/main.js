bc3fd511a093d6e4b4bdc853d2d374869a476d3f
sheeptipper <mzhang527@gmail.com>
2018-12-11 12:11:10 -0600

    final code testing

--- www/js/main.js
+++ www/js/main.js
@@ -10,8 +10,10 @@ var textToPrint2;
 var accel;
 var markov;
 var accuracy;
-var r, g, b;
+var r, g, b;;
 
+var sen = ""
+var markovReady = false;
 
 
 fetch('Nietzsche.txt')
@@ -23,6 +25,10 @@ fetch('Nietzsche.txt')
     //code to set up rita markov chain model
     markov = new RiMarkov(4);
     markov.loadText(a);
+    markovReady = true;
+    //console.log(a)
+
+
 
   });
 
@@ -40,23 +46,24 @@ fetch('Nietzsche.txt')
     textSize(50)
     textAlign(CENTER)
     //translate teh orgin of the grid to the center of the screen
-    translate(width/2, height/2)
+    translate(width/2, width/3)
     // text("lat: " + lat + "\n" + "lon: " + lon, -200, -200, 400,400);
 
     //set the variable distance to the result of the proximity function
 
 
-    //here, we assemble the text for display
+        //here, we assemble the text for display
 
-    textToPrint ="I AM " +"" ;
-    textToPrint1 ="I WANT ";
-    textToPrint2 ="I LOVE ";
+        textToPrint ="I AM " + sen ;
+        textToPrint1 ="I WANT " + sen;
+        textToPrint2 ="I LOVE " + sen;
 
 
-    //here we print the text for display
-    text(textToPrint, -width/2, r, width, height)
-    text(textToPrint1, -width/2, g, width, height)
-    text(textToPrint2, -width/2, b, width, height)
+        //here we print the text for display
+        text(textToPrint, -width/2, r, width, height)
+        text(textToPrint1, -width/2, g, width, height)
+        text(textToPrint2, -width/2, b, width, height)
+
 }
 
 
@@ -67,5 +74,11 @@ fetch('Nietzsche.txt')
       r = random(0, 255);
       g = random(0, 255);
       b = random(0, 255);
+      if(markovReady == true) {
+          sen = markov.generateSentences(1);
+          //textToPrint = sen
+
+          console.log("sen " + sen)
 
     }
+}
