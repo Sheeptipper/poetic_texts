0ccb15e74c91e40538cb10846c8a386c5c6c378b
sheeptipper <mzhang527@gmail.com>
2018-12-12 13:02:32 -0600

    changed server port

--- server.js
+++ server.js
@@ -3,14 +3,30 @@
 // see the README.md file in the root directory of this repo for instructions
 const express = require('express')
 
+var fs = require('fs')
+var https = require('https')
 // here we call the express() function which returns a default express server
 // application. we assign it to a variable called 'app'
 const app = express()
 
+/*app.get('/', function (req, res) {
+  res.send('hello world')
+})*/
+
+app.use( express.static(__dirname+'/www') )
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(443, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:443/')
+})
+
 // here we create a variable that will contain our port number, this is set
 // either in the terminal when we launch the app like: node server.js 3000
 // otherwise  defaults to port 80, which requires sudo, ex: sudo node server.js
-const port = process.argv[2] || 80
+/*const port = process.argv[2] || 80
 
 // we can serve up an entire directory of static files using express.static()
 // instead of having to define all the app.get() paths individually
@@ -23,4 +39,4 @@ app.listen( port, function(err){
     } else { // otherwise, log the following...
         console.log(`server is listening, visit http://localhost:${port}`)
     }
-})
+})*/
